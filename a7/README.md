76a7e2b3f86f282fdfce2f0510e92a9691f9f15e
harsh-not-haarsh <anand.2@iitj.ac.in>
2021-05-04 20:47:21 +0530

    added readme

---
+++ a7/README.md
@@ -0,0 +1,23 @@
+# Assignment 7
+
+## Compiling and Executing:
+- ```g++ main.cpp && ./a.out > Result.txt```
+
+## Interpreting Output
+- ```P0: page 9, TLB miss → page fault → free frame: 0 allocated to it.```: This means
+Process 0 requested for page 9 which lead to a TBL miss and a page fault. A free frame (frame 0) was found and was allocated to the page.
+
+- ```P0: page 9, TLB miss → page table valid → with frame no. 0```: This means
+Process 0 requested for page 9 which lead to a TBL miss. The page was found in the page table and frame 0 was allocated to it.
+
+- ```P0: page 3, TLB miss → page fault → frame: 0 re-allocated to it. (previously alloc to pg: 9)```: This means
+Process 0 requested for page 3 which lead to a TBL miss and a page fault. Frame 0 was then allocated to it, (where frame 0 was previously allocated to page 9)
+
+- ```P0: page 3, TLB hit with frame no. 0```: This means
+Process 0 requested for page 3, it was a TBL hit and the page can be found on frame 0.
+
+## Assumptions
+- 
+
+## Bugs
+- No known bugs.
--- a7/main.cpp
+++ a7/main.cpp
@@ -9,30 +9,43 @@ using namespace std;
 #define rep(i,a,b) for(int i=(a);i<(b);i++)
 #define repp(i,n) for(int i=(0);i<(n);i++)
 
-#define k 10
+#define k 2
 #define m 10
-#define f 10
-#define s 10
+#define f 5
+#define s 2
 #define pageRange 10 // should be same as m
 
 int allocated_memory[k];
 
-int pageTable[k][m][2]; // [process, page] valid, frame
+int pageTable[k][m][2]; // [process id, page num] -> valid, frame
 
 vector<vector<int>> tlb; // process id, pg, frame
 
-vector<int> freeFrames;
+vector<int> freeFrames; // list of frames that are free
 vector<vector<int>> occupiedFrames; // frame id, process id, pg
 
+void initTLB(){
+    repp(i, s){
+        tlb.push_back({rand()%k, rand()%pageRange, rand()%f});
+        // cerr<<tlb[i][0]<<" "<<tlb[i][1]<<" "<<tlb[i][2]<<endl;
+    }
+}
+
+void initFrames(){
+    repp(i, f) freeFrames.push_back(i);
+}
+
 void run_process(int id){
+    
     int mi = 1 + rand()%m;
     int req_len = (rand()%(8*mi)) + 2*mi;    
 
     repp(i, req_len){
         int pg = rand()%pageRange;
+        
 
-        cout<<"Process P"<<id<<": for page reference "<<pg;
-
+        cout<<"P"<<id<<": page "<<pg;
+        
         bool foundInTLB = false;
         repp(j, s){
             if(tlb[j][0] == id && tlb[j][1] == pg){
@@ -50,7 +63,7 @@ void run_process(int id){
         if(!foundInTLB){
             if(pageTable[id][pg][0]==1){
                 int allocFrame = pageTable[id][pg][1];
-                cout<<"TLB miss → page table valid → with frame no. "<<allocFrame<<endl;
+                cout<<", TLB miss → page table valid → with frame no. "<<allocFrame<<endl;
 
                 tlb.erase(tlb.begin());
                 tlb.push_back({id, pg, allocFrame});
@@ -58,7 +71,7 @@ void run_process(int id){
             } else {
                 if(freeFrames.size()>0){
                     int allocFrame = freeFrames[0];
-                    cout<<"TLB miss → page fault → free frame allocated frame no: "<<allocFrame<<endl;
+                    cout<<", TLB miss → page fault → free frame: "<<allocFrame<<" allocated to it."<<endl;
 
                     freeFrames.erase(freeFrames.begin());
 
@@ -75,7 +88,7 @@ void run_process(int id){
                     int old_process = occupiedFrames[0][0];
                     int old_pg = occupiedFrames[0][1];
                     int allocFrame = occupiedFrames[0][2];
-                    cout<<"TLB miss → page fault → frame re-allocated frame no: "<<allocFrame<<endl;
+                    cout<<", TLB miss → page fault → frame: "<<allocFrame<<" re-allocated to it. (previously alloc to pg: "<<old_pg<<')'<<endl;
 
                     occupiedFrames.erase(occupiedFrames.begin());
 
@@ -100,8 +113,11 @@ int main(){
 
     int num_exec = 0;
 
+    initTLB();
+    initFrames();
+
     while(num_exec < k){
-        run_process(rand()%k);
+        run_process(num_exec);
         num_exec++;
     }
     
